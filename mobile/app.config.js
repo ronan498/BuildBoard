9e6b967d9ac4f4fe3bb27e707f6fcdbaa1952622
Ronan Banerji <ronan.banerji@gmail.com>
2025-09-12 16:48:44 +0000

    upgrade dependencies

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -3,8 +3,13 @@ import 'dotenv/config';
 export default ({ config }) => {
   const apiUrl = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:4000';
   process.env.EXPO_PUBLIC_API_BASE_URL = apiUrl;
+
   return {
     ...config,
     extra: { ...config.extra, apiUrl },
+    plugins: [
+      ...(config.plugins || []),
+      'expo-font',
+    ],
   };
 };
